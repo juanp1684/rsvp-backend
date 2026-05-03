ca8885bc554102864b2f409e790ac71e6cc99052
Juan Pablo Chavez Arteaga <juanpablochavez@ravn.co>
2026-05-03 19:00:54 -0400

    Add wildcard subdomain CORS support via CORS_ALLOW_SUBDOMAINS flag

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,12 @@ return [
 
     'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000'))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => env('CORS_ALLOW_SUBDOMAINS')
+        ? array_map(function ($origin) {
+            preg_match('#^(https?://)(.+)$#', trim($origin), $m);
+            return '#^' . $m[1] . '[a-z0-9-]+\.' . preg_quote($m[2], '#') . '$#';
+        }, array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', ''))))
+        : [],
 
     'allowed_headers' => ['*'],
 
